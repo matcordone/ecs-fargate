2386330c5af2c749420906c6ccde15fde3e956b6
Mateo Cordone <matcordone@gmail.com>
2025-06-11 14:17:06 -0300

    Finish Network

---
+++ modules/network/outputs.tf
@@ -0,0 +1,18 @@
+output "name" {
+  value = aws_vpc.vpc_1.id
+}
+output "name_pub_subnet" {
+  value = aws_subnet.subnet_pub.id
+}
+output "name_priv_subnet_1" {
+  value = aws_subnet.subnet_1.id
+}
+output "name_priv_subnet_2" {
+  value = aws_subnet.subnet_2.id
+}
+output "sg_alb" {
+  value = aws_security_group.sg_alb.id
+}
+output "sg_ecs" {
+    value = aws_security_group.sg_ecs.id
+}
