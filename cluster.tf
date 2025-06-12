bffbe6550b2cd5be0e3528d45a384e58801f67c5
Mateo Cordone <matcordone@gmail.com>
2025-06-12 14:01:12 -0300

    policy add

--- cluster.tf
+++ cluster.tf
@@ -26,3 +26,9 @@ resource "aws_iam_policy_attachment" "ecs_task_execution_policy" {
   roles      = [aws_iam_role.ecs_task_execution_role.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_policy_attachment" "ecs_exec_managed" {
+  name       = "ECSExecManagedPolicyAttachment"
+  roles      = [aws_iam_role.ecs_task_execution_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
