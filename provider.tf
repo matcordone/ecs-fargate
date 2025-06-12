6c401d434050a75a5dea32150baeb567fa79c658
Mateo Cordone <matcordone@gmail.com>
2025-06-12 15:02:23 -0300

    Project end

--- provider.tf
+++ provider.tf
@@ -10,3 +10,17 @@ provider "aws" {
   region = "us-east-1"
 
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-backend-bucket290805"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    use_lockfile = true
+    
+  }
+}
+
+resource "aws_s3_bucket" "backend_bucket" {
+  bucket = "my-terraform-backend-bucket290805"
+}
